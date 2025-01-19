d432fb8c751e24010ad96ac39ad10e4b7b4eea7c
LepharamRamchiary <lepharamchiary@gmail.com>
2025-01-19 12:51:38 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navigation from "./customer/components/Navigation/Navigation";
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <div>Home</div>
+    </div>
+  );
+}
+
+export default App;
